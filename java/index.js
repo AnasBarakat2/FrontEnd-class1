d29b7a3032c9788db3376ff2d46d01e0e149622c
AnasBarakat2 <anasbarakat92@gmail.com>
2025-11-24 18:21:11 +0300

    updat

---
+++ java/index.js
@@ -0,0 +1,24 @@
+
+var username ="Anas"
+var age = 27
+var id 
+var work =null
+
+var info ={city:'irbid',phone:962}
+var mark=[20,40,60]
+
+
+console.log( username)
+console.log( age)
+console.log( id)
+console.log( info)
+console.log( work)
+console.log(mark[1])
+
+let Lname ="Barakat"
+Lname="M.Barakat"
+console.log(Lname)
+
+const x=10
+   
+console.log(x)
