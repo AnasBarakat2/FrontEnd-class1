818390b7f3d0c0554033de3532b4cb6f38bc97e3
AnasBarakat2 <anasbarakat92@gmail.com>
2025-12-03 17:59:41 +0300

    update

--- java/index.js
+++ java/index.js
@@ -23,32 +23,50 @@
    
 // console.log(x)
 
-var nav=document.getElementById('navigatbar')
-nav.style.backgroundColor='red'
-nav.style.borderRadius='50px'
+// var nav=document.getElementById('navigatbar')
+// nav.style.backgroundColor='red'
+// nav.style.borderRadius='50px'
 
-var imge=document.getElementsByClassName('imgGrid')[0]
-console.log(imge)
-imge.src="https://minimoon-store.com/wp-content/uploads/2022/08/Untitled-6.jpg"
+// var imge=document.getElementsByClassName('imgGrid')[0]
+// console.log(imge)
+// imge.src="https://minimoon-store.com/wp-content/uploads/2022/08/Untitled-6.jpg"
 
-var r=0
-if(r==9){
-    console.log('r is equal 9')
-}
-else{
-    console.log('r not equal 9')
-}
+// var r=0
+// if(r==9){
+//     console.log('r is equal 9')
+// }
+// else{
+//     console.log('r not equal 9')
+// }
 
-switch (r) {
-    case 9:
-         console.log('r is equal 9')
-        break;
-    case 10:
-         console.log('r is equal 10')
-        break;
-    default:
+// switch (r) {
+//     case 9:
+//          console.log('r is equal 9')
+//         break;
+//     case 10:
+//          console.log('r is equal 10')
+//         break;
+//     default:
          
-         console.log('r is nut defined')
-        break;
-        break;
+//          console.log('r is nut defined')
+//         break;
+//         break;
+// }
+
+var body =document.getElementById('tbody') /* بدنا نستدعي البودي بال id */
+var Button = document.getElementById('btn')
+
+function changetheme(){
+    if (body.style.backgroundColor=='white'){
+        body.style.backgroundColor = 'black' /*بتغير لون الخلفية للبودي */
+        Button.innerText ='change to white' /* عشان نعدل الكلام بداخل الزر */
+        
+    }
+    else{
+        body.style.backgroundColor='white'
+         Button.innerText ='change to black'
+    }
 }
+
+localStorage.setItem('Name','Anas')
+
